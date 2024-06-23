c44ad9f8a9d69419b42194a00bbc975ba061989a
Jeremy <ogprago@gmail.com>
2024-06-23 19:21:47 -0400

    Added and completed getSalesContractsById(). Added methods for SalesContractDao and LeaseContractDao

--- src/main/java/com/ps/dealership_api_starter/controllers/SalesContractsController.java
+++ src/main/java/com/ps/dealership_api_starter/controllers/SalesContractsController.java
@@ -1,4 +1,34 @@
 package com.ps.dealership_api_starter.controllers;
 
+import com.ps.dealership_api_starter.data.SalesContractDao;
+import com.ps.dealership_api_starter.models.SalesContract;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("salescontracts")
+@CrossOrigin
 public class SalesContractsController {
+    private SalesContractDao salesContractDao;
+
+    @Autowired
+    public SalesContractsController(SalesContractDao salesContractDao) {
+        this.salesContractDao = salesContractDao;
+    }
+
+    @GetMapping("{id}")
+    public SalesContract getSalesContractsById(@PathVariable int id) {
+        try {
+            var product = salesContractDao.getSalesContractsById(id);
+
+            if (product == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+            return product;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
 }
--- src/main/java/com/ps/dealership_api_starter/data/mysql/MySqlSalesContractDao.java
+++ src/main/java/com/ps/dealership_api_starter/data/mysql/MySqlSalesContractDao.java
@@ -1,4 +1,61 @@
 package com.ps.dealership_api_starter.data.mysql;
 
-public class MySqlSalesContractDao {
+import com.ps.dealership_api_starter.data.SalesContractDao;
+import com.ps.dealership_api_starter.models.Dealership;
+import com.ps.dealership_api_starter.models.SalesContract;
+import com.ps.dealership_api_starter.models.Vehicle;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MySqlSalesContractDao extends MySqlDaoBase implements SalesContractDao {
+    public MySqlSalesContractDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public SalesContract getSalesContractsById(int contractId) {
+        String sql = "SELECT * FROM sales_contracts WHERE contract_id = ?;";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, contractId);
+
+            ResultSet row = preparedStatement.executeQuery();
+
+            if (row.next()) {
+                return mapRow(row);
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public SalesContract addSalesContracts(SalesContract salesContract) {
+        return null;
+    }
+
+    protected static SalesContract mapRow(ResultSet row) throws SQLException {
+         int contractId = row.getInt("contract_id");
+         String contractDate = row.getString("contract_date");
+         String customerName = row.getString("customer_name");
+         String customerEmail = row.getString("customer_email");
+         int vin = row.getInt("vin");
+         double salesTax = row.getDouble("sales_tax");
+         double recordingFee = row.getDouble("recording_fee");
+         double processingFee = row.getDouble("processing_fee");
+         double totalPrice = row.getDouble("total_price");
+         String financeOption = row.getString("finance_option");
+         double monthlyPayment = row.getDouble("monthly_payment");
+
+        return new SalesContract(contractId, contractDate, customerName, customerEmail, vin, salesTax,recordingFee, processingFee, totalPrice, financeOption, monthlyPayment);
+    }
 }
